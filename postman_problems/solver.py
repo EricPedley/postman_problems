e7ef5a8270851057f17b1c8d33ecfaf623b24eef
Eric Pedley <ericpedley@gmail.com>
2024-04-20 14:03:43 -0700

    fix miscellaneous bullshit

--- postman_problems/solver.py
+++ postman_problems/solver.py
@@ -1,4 +1,4 @@
-import itertools
+from itertools import combinations
 import logging
 import networkx as nx
 
@@ -51,7 +51,7 @@ def rpp(edgelist_filename, start_node=None, edge_weight='distance', verbose=Fals
 
     logger_rpp.info('Find min weight matching using blossom algorithm')
     g_odd_complete = create_complete_graph(odd_node_pairs_shortest_paths, flip_weights=True)
-    odd_matching = nx.algorithms.max_weight_matching(g_odd_complete, True)
+    odd_matching = dedupe_matching(nx.algorithms.max_weight_matching(g_odd_complete, True))
 
     logger_rpp.info('add the min weight matching edges to g')
     g_aug = add_augmenting_path_to_graph(g_req, odd_matching)
@@ -62,7 +62,7 @@ def rpp(edgelist_filename, start_node=None, edge_weight='distance', verbose=Fals
     return circuit, g_full
 
 
-def cpp(edgelist_filename, start_node=None, edge_weight='distance', verbose=False):
+def cpp(g: nx.Graph, start_node=None, edge_weight="distance", verbose=False):
     """
     Solving the CPP from beginning (load network data) to end (finding optimal route).
     Can be run from command line with arguments from cpp.py, or from an interactive Python session (ex jupyter notebook)
@@ -81,25 +81,21 @@ def cpp(edgelist_filename, start_node=None, edge_weight='distance', verbose=Fals
           The third element is the dict of edge attributes for that edge.
         The original graph is returned as well.  This is needed for visualization
     """
-    logger_cpp.disabled = not verbose
-
-    logger_cpp.info('read edgelist and create base graph')
-    el = read_edgelist(edgelist_filename, keep_optional=False)
-    g = create_networkx_graph_from_edgelist(el)
-
-    logger_cpp.info('get augmenting path for odd nodes')
     odd_nodes = get_odd_nodes(g)
-    odd_node_pairs = list(itertools.combinations(odd_nodes, 2))
-    odd_node_pairs_shortest_paths = get_shortest_paths_distances(g, odd_node_pairs, edge_weight)
-    g_odd_complete = create_complete_graph(odd_node_pairs_shortest_paths, flip_weights=True)
+    odd_node_pairs = list(combinations(odd_nodes, 2))
+    odd_node_pairs_shortest_paths = get_shortest_paths_distances(
+        g, odd_node_pairs, edge_weight
+    )
+    g_odd_complete = create_complete_graph(
+        odd_node_pairs_shortest_paths, flip_weights=True
+    )
 
-    logger_cpp.info('Find min weight matching using blossom algorithm')
-    odd_matching = nx.algorithms.max_weight_matching(g_odd_complete, True)
+    odd_matching = dedupe_matching(
+        nx.algorithms.max_weight_matching(g_odd_complete, True)
+    )
 
-    logger_cpp.info('add the min weight matching edges to g')
     g_aug = add_augmenting_path_to_graph(g, odd_matching)
 
-    logger_cpp.info('get eulerian circuit route')
     circuit = list(create_eulerian_circuit(g_aug, g, start_node))
 
     return circuit, g
